a7bfcba50816e57dee3072638a806b3f556eafe1
Arsh Verma <arshverma.dev@gmail.com>
2026-01-08 20:35:10 +0530

    Create page.tsx

---
+++ src/app/(dashboard)/admin/explorer/page.tsx
@@ -0,0 +1,9 @@
+import { UniversalRegistry } from "@/components/admin/UniversalRegistry"
+
+export default function ExplorerPage() {
+  return (
+    <div className="p-4 md:p-8">
+      <UniversalRegistry />
+    </div>
+  )
+}
